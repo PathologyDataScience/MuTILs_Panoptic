b9089197a05759788c6267276e6c94a6ac861b8f
kheffah <mohamed.amgad01@gmail.com>
2023-05-15 02:28:42 -0500

    add mutils trainer

---
+++ utils/MiscRegionUtils.py
@@ -0,0 +1,356 @@
+import os
+from os.path import join as opj
+import torch
+import numpy as np
+from pandas import DataFrame
+from scipy import ndimage
+from skimage.feature import peak_local_max
+from skimage.segmentation import watershed
+from skimage.morphology import binary_opening, disk
+from scipy.ndimage import distance_transform_edt
+from typing import Dict
+
+from MuTILs_Panoptic.utils.GeneralUtils import (
+    load_configs, reverse_dict, _div, abserr
+)
+from MuTILs_Panoptic.mutils_panoptic.MuTILs import MuTILs
+from MuTILs_Panoptic.utils.TorchUtils import load_torch_model, t2np
+from MuTILs_Panoptic.configs.region_model_configs import MuTILsParams
+from MuTILs_Panoptic.utils.torchvision_transforms import PILToTensor
+
+# =============================================================================
+# Constants
+
+# map np dtypes to vips
+NP2VIPS_DTYPES = {
+    'uint8': 'uchar',
+    'int8': 'char',
+    'uint16': 'ushort',
+    'int16': 'short',
+    'uint32': 'uint',
+    'int32': 'int',
+    'float32': 'float',
+    'float64': 'double',
+    'complex64': 'complex',
+    'complex128': 'dpcomplex',
+}
+
+VIPS2NP_DTYPES = {
+    'uchar': np.uint8,
+    'char': np.int8,
+    'ushort': np.uint16,
+    'short': np.int16,
+    'uint': np.uint32,
+    'int': np.int32,
+    'float': np.float32,
+    'double': np.float64,
+    'complex': np.complex64,
+    'dpcomplex': np.complex128,
+}
+
+# =============================================================================
+
+
+def numpy2vips(arr):
+    """numpy array to vips image"""
+    import pyvips
+
+    height, width, bands = arr.shape
+    linear = arr.reshape(width * height * bands)
+    vi = pyvips.Image.new_from_memory(
+        linear.data, width, height,
+        bands,
+        NP2VIPS_DTYPES[str(arr.dtype)],
+    )
+    return vi
+
+
+def vips2numpy(vim):
+    """vips image to numpy array."""
+    return np.ndarray(
+        buffer=vim.write_to_memory(),
+        dtype=VIPS2NP_DTYPES[vim.format],
+        shape=[vim.height, vim.width, vim.bands]
+    )
+
+
+def pil2tensor(*args, **kwargs):
+    """A functional wrapper areound PILToTensor"""
+    return PILToTensor()(*args, **kwargs)
+
+
+def get_configured_logger(logdir: str, prefix="", toscreen=True, tofile=True):
+    """"""
+    import logging
+    import datetime
+    assert any((toscreen, tofile)), "must log to screen and/or to file!"
+    now = str(datetime.datetime.now()).replace(' ', '_')
+    logfile = opj(logdir, f'{prefix}_{now}.log')
+    handlers = []
+    if toscreen:
+        handlers.append(logging.StreamHandler())  # print to console
+    if tofile:
+        handlers.append(logging.FileHandler(logfile))  # save to log file
+    # noinspection PyArgumentList
+    logging.basicConfig(level=logging.INFO, handlers=handlers)
+    logger = logging.getLogger("")
+    return logger
+
+
+def load_region_configs(configs_path, warn=True):
+    """"""
+    if os.path.exists(configs_path):
+        if warn:
+            input(f"Loading existing configs from: {configs_path}. Continue?")
+        cfg = load_configs(configs_path=configs_path)
+    else:
+        print("Loading default configs")
+        import ctme.configs.region_model_configs as cfg
+    return cfg
+
+
+def load_trained_mutils_model(ckpt_path: str, mtp: MuTILsParams):
+    """"""
+    # GPU vs CPU
+    iscude = torch.cuda.is_available()
+    device = torch.device('cuda') if iscude else torch.device('cpu')
+
+    # init model
+    model = MuTILs(**mtp.model_params)
+    model.to(device)
+
+    # load weights
+    ckpt = load_torch_model(checkpoint_path=ckpt_path, model=model)
+    model = ckpt['model']
+    model.eval()
+
+    return model
+
+
+def logits2preds(logits, return_probabs=False, return_aggregate=False):
+    """
+    Convert logits to argmaxed predictions. If the first axis of logits
+    has more than one image, it is assumed that these are multiple
+    augmentations of the same image and their probabilities are aggregated.
+
+    Parameters
+    ----------
+    logits: torch.Tensor
+        a tensor of shape (n_augmentations, n_classes, m, n)
+    return_probabs: bool
+        also return prediction probabilities?
+    return_aggregate: bool
+        also return the mean of the per-pixel probab? This
+        serves as a proxy for overall model confidence for this particular
+        roi/hpf.
+
+    Returns
+    -------
+    np.array
+        argmaxed predictions, a uint8 np array of shape (m, n)
+    np.array, optional
+        prediction probabs, an np array of shape (n_classes, m, n)
+
+    """
+    assert logits.ndim == 4, "first axis must be the image index"
+    if logits.shape[0] == 1:
+        agg_logits = logits[0, ...]
+    else:
+        agg_logits = torch.softmax(logits, 1)
+        agg_logits = agg_logits.sum(0)
+
+    if return_probabs or return_aggregate:
+        agg_logits = torch.softmax(agg_logits, 0)
+
+    preds = t2np(agg_logits.argmax(0) + 1)
+
+    out = [np.uint8(preds)]
+    if return_probabs:
+        out.append(t2np(agg_logits))
+    if return_aggregate:
+        out.append(float(t2np(agg_logits).max(0).mean()))
+
+    return out
+
+
+def _pixsum(msk: np.ndarray, code: int) -> int:
+    """"""
+    return int(np.sum(0 + (msk == code)))
+
+
+def summarize_region_mask(
+    mask: np.ndarray, rcd: Dict[str, int], prefix="pixelCount_"
+) -> Dict[str, int]:
+    """Summarize no. of pixels from various classes in region mask."""
+    return {f"{prefix}{r}": _pixsum(mask, cd) for r, cd in rcd.items()}
+
+
+def get_objects_from_binmask(
+    binmask,
+    *,
+    open_first=True,
+    selem=None,
+    mindist=5,
+    use_watershed=True,
+    minpixels=10,
+    maxpixels=None,
+    _return_codes=True,  # unique object codes
+):
+    """
+    Given a binary mask, get object mask where pixel values encode
+    individual object membership.
+    """
+    # binary opening helps isolate touching objects
+    if open_first:
+        binmask = binary_opening(
+            binmask, selem=selem if selem is not None else np.ones((5, 5))
+        )
+
+    # watershed also helps isolate touching objects
+    if use_watershed:
+        # compute the exact Euclidean distance from every binary
+        # pixel to the nearest zero pixel, then find peaks in this
+        # distance map
+        binmask = binmask * 255  # scipy prefers 255
+        D = ndimage.distance_transform_edt(binmask)
+        localmax = np.zeros(binmask.shape, dtype=bool)
+        peak_idxs = peak_local_max(D, min_distance=mindist, labels=binmask)
+        localmax[peak_idxs[:, 0], peak_idxs[:, 1]] = True
+
+        # perform a connected component analysis on the local peaks,
+        # using 8-connectivity, then appy the Watershed algorithm
+        markers = ndimage.label(localmax, structure=np.ones((3, 3)))[0]
+        labels = watershed(-D, markers, mask=binmask, watershed_line=True)
+
+    else:
+        labels, _ = ndimage.label(binmask, structure=np.ones((3, 3)))
+
+    # filter artifacts
+    unique, counts = np.unique(labels, return_counts=True)
+    if unique[0] == 0:
+        unique = unique[1:]
+        counts = counts[1:]
+    keep = counts >= minpixels
+    if maxpixels is not None:
+        keep2 = counts < maxpixels
+        keep = keep & keep2
+    discard_vals = unique[~keep]
+    labels[np.isin(labels, discard_vals)] = 0
+
+    if not _return_codes:
+        return labels
+
+    return labels, unique[keep]
+
+
+def get_region_within_x_pixels(
+    center_mask: np.ndarray,
+    surround_mask: np.ndarray,
+    max_dist: int = 32,
+    min_ref_pixels: int = None,
+):
+    """
+    Get mask of regions from surround_mask within x pixels from center_mask.
+
+    Parameters
+    ----------
+    center_mask: np.ndarray
+        Binary mask of reference region (eg tumor)
+    surround_mask: np.ndarray
+        Binary mask of surround region (eg stroma).
+        Must be same shape as `center_mask`
+    max_dist: int
+        Max distance in pixels from reference region.
+    min_ref_pixels: int, default is None
+        Min no of pixels to consider a reference region. If none, all
+        reference regions are analyzed.
+
+    Returns
+    -------
+    np.ndarray
+        mask of surround region within max_dist of reference region
+        for example, this could be the peri-tumoral stroma. It has the same
+        shape as `center_mask`.
+
+    """
+    # open to remove small objects
+    if min_ref_pixels is not None:
+        center_mask = binary_opening(center_mask, selem=disk(min_ref_pixels))
+    # find proximity of each pixel from center regions (our reference)
+    shape = center_mask.shape
+    maxd = np.sqrt(np.sum([side ** 2 for side in shape]))
+    if np.sum(0 + center_mask) > 5:
+        distance_from_ref = distance_transform_edt(~center_mask)
+        distance_from_ref[distance_from_ref > max_dist] = maxd
+    else:
+        distance_from_ref = maxd + np.zeros(shape, dtype=np.float32)
+    # get mask of surround within x pixels from the reference region
+    surround = distance_from_ref < maxd
+    surround[~surround_mask] = False
+
+    return surround
+
+
+def summarize_nuclei_mask(obj2lbl: dict, ncd: dict):
+    """Summarize HPF nuclei mask."""
+    nst = "nNuclei"
+    realn = [k for k in ncd.keys() if k not in ['EXCLUDE', 'BACKGROUND']]
+    lbl2objs = {lbl: [] for lbl in realn}
+    lbl2objs.update(reverse_dict(obj2lbl, preserve=True))
+    out = {f"{nst}_all": len(obj2lbl)}
+    out.update({
+        f"{nst}_{cls}": len(lbl2objs[cls]) for cls in ncd if cls in lbl2objs
+    })
+    return out
+
+
+def _aggregate_semsegm_stats(df: DataFrame, colpfx='', supermap=None):
+    """"""
+    # # Must be passed explicitely!!
+    # supermap = {
+    #     'NORMAL': 'TUMOR',
+    #     'JUNK': 'OTHER',
+    #     'BLOOD': 'OTHER',
+    #     'WHITE': 'OTHER',
+    # }
+    if supermap is not None:
+        discard = []
+        for cat, supercat in supermap.items():
+            for sfx in [
+                'pixel_intersect', 'pixel_count', 'segm_intersect', 'segm_sums'
+            ]:
+                df.loc[:, f'roi-regions_{supercat}-{sfx}'] += (
+                    df.loc[:, f'roi-regions_{cat}-{sfx}']
+                )
+                discard.append(f'roi-regions_{cat}-{sfx}')
+        df = df.loc[:, [c for c in df.columns if c not in discard]]
+
+    agg = {}
+    # calculate overall accuracy
+    for intersect in [j for j in df.columns if '-pixel_intersect' in j]:
+        total = intersect.replace('_intersect', '_count')
+        agg[intersect] = df.loc[:, intersect].sum()
+        agg[total] = df.loc[:, total].sum()
+        agg[intersect.replace('_intersect', '_accuracy')] = \
+            _div(agg[intersect], agg[total])
+    # calculate iou and dice
+    for intersect in [j for j in df.columns if '-segm_intersect' in j]:
+        total = intersect.replace('_intersect', '_sums')
+        agg[intersect] = df.loc[:, intersect].sum()
+        agg[total] = df.loc[:, total].sum()
+        agg[intersect.replace('_intersect', '_iou')] = \
+            _div(agg[intersect], agg[total] - agg[intersect])
+        agg[intersect.replace('_intersect', '_dice')] = \
+            _div(2. * agg[intersect], agg[total])
+    # calculate tils score
+    for pf in ['roi', 'hpf']:
+        pfx = f'{colpfx}{pf}-CTA-score'
+        for sfx in ['true', 'pred']:
+            numer = f'{pfx}_numer_{sfx}'
+            denom = f'{pfx}_denom_{sfx}'
+            agg[numer] = df.loc[:, numer].sum()
+            agg[denom] = df.loc[:, denom].sum()
+            agg[f'{pfx}_{sfx}'] = _div(agg[numer], agg[denom])
+        agg[f'{pfx}_abserror'] = abserr(agg[f'{pfx}_pred'], agg[f'{pfx}_true'])
+
+    return agg
---
+++ utils/TorchUtils.py
@@ -0,0 +1,123 @@
+import os
+import pickle
+from warnings import warn
+import torch
+
+import MuTILs_Panoptic.utils.torchvision_transforms as tvdt
+
+
+ISCUDA = torch.cuda.is_available()
+
+
+def tensor_isin(arr1, arr2):
+    r""" Compares a tensor element-wise with a list of possible values.
+    See :func:`torch.isin`
+
+    Source: https://github.com/pytorch/pytorch/pull/26144
+    """
+    result = (arr1[..., None] == arr2).any(-1)
+    return result.type(torch.ByteTensor)
+
+
+def transform_dlinput(
+        tlist=None, make_tensor=True, flip_prob=0.5,
+        augment_stain_sigma1=0.5, augment_stain_sigma2=0.5):
+    """Transform input image data for a DL model.
+
+    Parameters
+    ----------
+    tlist: None or list. If testing mode, pass as None.
+    flip_prob
+    augment_stain_sigma1
+    augment_stain_sigma2
+
+    """
+    tmap = {
+        'hflip': tvdt.RandomHorizontalFlip(prob=flip_prob),
+        'augment_stain': tvdt.RandomHEStain(
+            sigma1=augment_stain_sigma1, sigma2=augment_stain_sigma2),
+    }
+    tlist = [] if tlist is None else tlist
+    transforms = []
+    # go through various transforms
+    for tname in tlist:
+        transforms.append(tmap[tname])
+    # maybe convert to tensor
+    if make_tensor:
+        # transforms.append(tvdt.PILToTensor(float16=ISCUDA))
+        transforms.append(tvdt.PILToTensor(float16=False))
+    return tvdt.Compose(transforms)
+
+
+def load_torch_model(checkpoint_path, model, optimizer=None):
+    """
+    Source https://towardsdatascience.com/how-to-save-and-load-a-model-in- ...
+    ... pytorch-with-a-complete-example-c2920e617dee
+
+    checkpoint_path: path to save checkpoint
+    model: model that we want to load checkpoint parameters into
+    optimizer: optimizer we defined in previous training
+    """
+    if torch.cuda.is_available():
+        extra = {}
+    else:
+        extra = {
+            'map_location': lambda storage, loc: storage,
+            # 'map_location': {'cuda:0': 'cpu'},
+        }
+
+    # load model state
+    try:
+        model.module.load_state_dict(torch.load(checkpoint_path, **extra))
+    except AttributeError:  # this is not a Dataparallel model
+        model.load_state_dict(torch.load(checkpoint_path, **extra))
+
+    # load optimizer state
+    if optimizer is not None:
+        optimizer_path = checkpoint_path.replace('.pt', '.optim')
+        if os.path.exists(optimizer_path):
+            optimizer.load_state_dict(torch.load(optimizer_path, **extra))
+        else:
+            warn(f"Existing optimizer not found at: {optimizer_path}")
+
+    to_return = {
+        'model': model,
+        'optimizer': optimizer,
+    }
+
+    # extra metadata (eg epoch)
+    metapath = checkpoint_path.replace('.pt', '.meta')
+    if os.path.exists(metapath):
+        try:
+            meta = pickle.load(open(metapath, 'rb'))
+        except:
+            meta = {'epoch': 0}
+        to_return.update(meta)
+
+    return to_return
+
+
+def get_optimizer(model, optimizer_type='SGD', optimizer_params=None):
+    params = [p for p in model.parameters() if p.requires_grad]
+    if optimizer_type == 'SGD':
+        optimizer_params = {
+            'lr': 0.005,
+            'momentum': 0.9,
+            'weight_decay': 0.0005,
+        } if optimizer_params is None else optimizer_params
+        optimizer = torch.optim.SGD(params, **optimizer_params)
+    elif optimizer_type == 'Adam':
+        optimizer_params = {
+            'lr': 1e-4,
+            'betas': (0.9, 0.999),
+            'eps': 1e-8,
+            'weight_decay': 0.0001,
+        } if optimizer_params is None else optimizer_params
+        optimizer = torch.optim.Adam(params, **optimizer_params)
+    else:
+        raise NotImplementedError(f'Unknown optimizer: {optimizer_type}')
+    return optimizer
+
+
+def t2np(x: torch.Tensor):
+    return x.detach().cpu().numpy()
