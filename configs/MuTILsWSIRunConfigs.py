a66aa953f916efd9e7d545e9d46b78b3af0b38ce
Akos Szolgyen <akos.szolgyen@northwestern.edu>
2024-11-22 12:47:14 -0600

    Create config.yaml

--- configs/MuTILsWSIRunConfigs.py
+++ configs/MuTILsWSIRunConfigs.py
@@ -1,31 +1,94 @@
 import os
 from os.path import join as opj
 from pandas import read_csv
+import yaml
 
+# Configure logger. This must be done BEFORE importing histolab modules
+from MuTILs_Panoptic.utils.MiscRegionUtils import get_configured_logger, load_region_configs
+from MuTILs_Panoptic.utils.GeneralUtils import splitlist
 
 HOME = os.path.expanduser('~')
-BASEPATH = opj(HOME, 'Desktop', 'cTME')
-
+FILE_PATH = os.path.dirname(os.path.abspath(__file__))
+BASE_CONFIG_FILE = opj(FILE_PATH, 'config.yaml')
 
 class BaseConfigs:
 
-    DEBUG = True  # IMPORTANT: debug??
+    def __init__(self, config_file) -> None:
+        self.logger = self.set_up_logger()
+        self.config_dict = self.load_config(config_file)
+        self.set_config_values()
 
-    # COHORT = 'NiceExamplesForMuTILsPreprint'
-    # COHORT = 'TCGA_BRCA'
-    COHORT = 'CPSII_40X'
-    # COHORT = 'CPS3_40X'
-    # COHORT = 'plco_breast'
+    def set_up_logger(self) -> object:
+        '''
+        Set up the logger for the class.
 
-    N_SUBSETS = 1
-    # N_SUBSETS = 8 if not DEBUG else 1
-    # N_SUBSETS = 16 if not DEBUG else 1
+        Returns:
+            object: The logger object.
+        '''
+        LOGDIR = opj(self.OUTPUT_PATH, 'LOGS')
+        os.makedirs(LOGDIR, exist_ok=True)
+        logger = get_configured_logger(
+            logdir=LOGDIR, prefix='MuTILsWSIRunner', tofile=True
+        )
 
-    MODELNAME = 'mutils_06022021'
+        return logger
 
 
-class GrandChallengeConfigs:
+    def load_config(self, file_path: str) -> dict:
+        """
+        Load configuration from a YAML file.
 
+        Parameters:
+            file_path (str): The path to the YAML configuration file.
+
+        Returns:
+            dict: The loaded configuration as a dictionary.
+
+        Raises:
+            FileNotFoundError: If the configuration file is not found.
+            yaml.YAMLError: If there is an error parsing the YAML file.
+        """
+        try:
+            with open(file_path, 'r') as ymlfile:
+                return yaml.load(ymlfile, Loader=yaml.FullLoader)
+        except FileNotFoundError as e:
+            self.logger.error(f"Configuration file not found: {file_path}")
+            raise e
+        except yaml.YAMLError as exc:
+            self.logger.error(f"Error parsing YAML file: {exc}")
+            raise exc
+
+    def set_config_values(self):
+        '''
+        Set the configuration values from the config file to the class attributes.
+
+        Raises:
+            KeyError: If any of the required configuration values are not set in the config file.
+        '''
+        required_keys = ['DEBUG', 'MODELNAME', 'COHORT', 'N_SUBSETS', 'BASEPATH',
+                         'INPUT_PATH', 'OUTPUT_PATH']
+        for key in required_keys:
+            if key not in self.config_dict:
+                self.logger.error(f"{key} not set in config file")
+                raise KeyError(f"{key} not set in config file")
+
+        self.DEBUG = self.config_dict['DEBUG']
+        self.MODELNAME = self.config_dict['MODELNAME']
+        self.COHORT  = self.config_dict['COHORT']
+        self.N_SUBSETS = self.config_dict['N_SUBSETS']
+        # self.N_SUBSETS = 8 if not self.DEBUG else 1
+        # self.N_SUBSETS = 16 if not self.DEBUG else 1
+        self.BASEPATH = self.config_dict['BASEPATH']
+        self.INPUT_PATH = self.config_dict['INPUT_PATH']
+        self.OUTPUT_PATH = self.config_dict['OUTPUT_PATH']
+        self.BASEMODELPATH = self.config_dict['BASEMODELPATH']
+
+class GrandChallengeConfigs(BaseConfigs):
+
+    def __init__(self, config_file) -> None:
+        super().__init__(config_file)
+
+    # TODO: Make proper class inheritance
     WORKPATH = opj(HOME, 'Desktop', 'TILS_CHALLENGE')
     INPUT_PATH = opj(WORKPATH, '1_INPUT', BaseConfigs.COHORT)
     OUTPUT_PATH = opj(WORKPATH, '2_OUTPUT', BaseConfigs.COHORT)
@@ -63,15 +126,19 @@ class GrandChallengeConfigs:
     }
 
 
-class FullcTMEConfigs:
+class FullcTMEConfigs(BaseConfigs):
 
+    def __init__(self, config_file) -> None:
+        super().__init__(config_file)
+
+    # TODO: Make proper class inheritance
     CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
     REPO_PATH = os.path.dirname(CURRENT_PATH)
 
     INPUT_PATH = opj(REPO_PATH,'data/input', BaseConfigs.COHORT)
     OUTPUT_PATH = opj(REPO_PATH,'data/output', BaseConfigs.COHORT)
     BASEMODELPATH = opj(
-        BASEPATH, 'results', 'mutils', 'models', BaseConfigs.MODELNAME
+        BaseConfigs.BASEPATH, 'results', 'mutils', 'models', BaseConfigs.MODELNAME
     )
 
     restrict_to_vta = False
@@ -89,12 +156,16 @@ class FullcTMEConfigs:
     vlres_scorer_kws = None
 
 
-class NiceExamplesForMuTILsPreprintConfigs:
+class NiceExamplesForMuTILsPreprintConfigs(BaseConfigs):
 
+    def __init__(self, config_file) -> None:
+        super().__init__(config_file)
+
+    # TODO: Make proper class inheritance
     INPUT_PATH = opj('/input', BaseConfigs.COHORT)
     OUTPUT_PATH = opj('/output', BaseConfigs.COHORT)
     BASEMODELPATH = opj(
-        BASEPATH, 'results', 'mutils', 'models', BaseConfigs.MODELNAME
+        BaseConfigs.BASEPATH, 'results', 'mutils', 'models', BaseConfigs.MODELNAME
     )
 
     restrict_to_vta = False
@@ -117,39 +188,24 @@ class RunConfigs:
     # IMPORTANT: THIS SWITCHES THE ANALYSIS TYPE
     # cfg = NiceExamplesForMuTILsPreprintConfigs
     # cfg = GrandChallengeConfigs
-    cfg = FullcTMEConfigs
-
-    os.makedirs(cfg.OUTPUT_PATH, exist_ok=True)
-
-    # Configure logger. This must be done BEFORE importing histolab modules
-    from MuTILs_Panoptic.utils.MiscRegionUtils import \
-        get_configured_logger, load_region_configs
-
-    LOGDIR = opj(cfg.OUTPUT_PATH, 'LOGS')
-    os.makedirs(LOGDIR, exist_ok=True)
-    LOGGER = get_configured_logger(
-        logdir=LOGDIR, prefix='MuTILsWSIRunner', tofile=True
-    )
+    cfg = FullcTMEConfigs(BASE_CONFIG_FILE)
 
     # model weights
     MODEL_PATHS = {}
     for f in range(1, 6):
-        MODEL_PATHS[f'{BaseConfigs.MODELNAME}-fold{f}'] = opj(
-            cfg.BASEMODELPATH, f'fold_{f}', f'{BaseConfigs.MODELNAME}_fold{f}.pt'
+        MODEL_PATHS[f'{cfg.MODELNAME}-fold{f}'] = opj(
+            cfg.BASEMODELPATH, f'fold_{f}', f'{cfg.MODELNAME}_fold{f}.pt'
         )
 
     # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     # Subdividing cohort to run on multiple docker instances
 
-    from MuTILs_Panoptic.utils.GeneralUtils import splitlist
-
     ALL_SLIDENAMES = os.listdir(cfg.INPUT_PATH)
 
     if BaseConfigs.COHORT.startswith('TCGA') and cfg.restrict_to_vta:
         # RESTRICT TO ROBERTO SALGADO ASSESSED SLIDES
-        from MuTILs_Panoptic.utils.GeneralUtils import splitlist
         SLIDENAMES = read_csv(opj(
-            BASEPATH, 'data', 'tcga-clinical',
+            cfg.BASEPATH, 'data', 'tcga-clinical',
             'PRIVATE_RSalgado_TCGA_TILScores.csv'
         )).iloc[:, 0].to_list()
         SLIDENAMES = [j[:12] for j in SLIDENAMES]
@@ -158,7 +214,7 @@ class RunConfigs:
     elif BaseConfigs.COHORT.endswith('CPS2') and cfg.restrict_to_vta:
         # RESTRICT TO TED ASSESSED SLIDES (CPS2)
         acs_vta = read_csv(
-            opj(BASEPATH, 'data', 'acs-clinical',
+            opj(cfg.BASEPATH, 'data', 'acs-clinical',
                 'CPSII_BRCA_FacilityIDs_20210331.csv'),
             index_col=0)
         acs_vta.rename(columns={'TILS_STR': 'vta'}, inplace=True)
@@ -170,7 +226,7 @@ class RunConfigs:
         SLIDENAMES = ALL_SLIDENAMES
         SLIDENAMES.sort()
         SLIDENAMES = splitlist(
-            SLIDENAMES, len(SLIDENAMES) // BaseConfigs.N_SUBSETS
+            SLIDENAMES, len(SLIDENAMES) // cfg.N_SUBSETS
         )
 
     # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -189,7 +245,7 @@ class RunConfigs:
         # roi_side_hres=512 if BaseConfigs.DEBUG else 1024,
         roi_side_hres=1024,
         discard_edge_hres=0,  # keep 0 -> slow + can't get the gap to be exact
-        logger=LOGGER,
+        logger=cfg.logger,
 
         # Defined in cfg
         save_wsi_mask=cfg.save_wsi_mask,
@@ -203,5 +259,5 @@ class RunConfigs:
         topk_salient_rois=cfg.topk_salient_rois,
         vlres_scorer_kws=cfg.vlres_scorer_kws,
 
-        _debug=BaseConfigs.DEBUG,
+        _debug=cfg.DEBUG,
     )
